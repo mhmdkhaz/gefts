2555c131e2282a613f36cc6898137807b2426c47
mhmdkhaz <mhmdkh0957@gmail.com>
2022-04-12 14:34:35 -0700

    add message and add employe and employe

--- js/dashbordAdmin.js
+++ js/dashbordAdmin.js
@@ -1,18 +1,18 @@
-let filterBtn = document.querySelectorAll(".dash .link-div");
-let filterLink = document.querySelectorAll(".dash .lind-div a");
-let filterDiv = document.querySelectorAll("#main .item");
+// let filterBtn = document.querySelectorAll(".dash .link-div");
+// let filterLink = document.querySelectorAll(".dash .lind-div a");
+// let filterDiv = document.querySelectorAll("#main .item");
 
-filterBtn.forEach(function (link) {
-  filterBtn.forEach((link) => {
-    link.addEventListener("click", manageImgs);
-  });
-});
+// filterBtn.forEach(function (link) {
+//   filterBtn.forEach((link) => {
+//     link.addEventListener("click", manageImgs);
+//   });
+// });
 
-function manageImgs() {
-  filterDiv.forEach(function (div) {
-    div.style.display = "none";
-  });
-  document.querySelectorAll(this.dataset.cat).forEach((el) => {
-    el.style.display = "block";
-  });
-}
+// function manageImgs() {
+//   filterDiv.forEach(function (div) {
+//     div.style.display = "none";
+//   });
+//   document.querySelectorAll(this.dataset.cat).forEach((el) => {
+//     el.style.display = "block";
+//   });
+// }
